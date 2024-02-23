913995062f179ad2d27b982966ee4833ea70326c
bogdan <tvorbogdan@gmail.com>
2024-02-23 18:05:13 +0200

    RA-04 Создание первого фрагмента с категориями - Исправлены нейминги ресурсов - Исправлена вёрстка - Исправлен viewbinding

--- app/src/main/java/com/example/recepiesapplication/MainActivity.kt
+++ app/src/main/java/com/example/recepiesapplication/MainActivity.kt
@@ -2,19 +2,20 @@ package com.example.recepiesapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
+import androidx.fragment.app.commit
 import com.example.recepiesapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(binding.mainContainer.id, CategoriesListFragment())
-            .commit()
+        supportFragmentManager.commit {
+            replace(binding.mainContainer.id, CategoriesListFragment())
+        }
     }
 }
