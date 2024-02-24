d3fd2bce6e25aff423de1f33d060e71eac23457e
bogdan <tvorbogdan@gmail.com>
2024-02-25 00:06:33 +0200

    RA-05 Настроена базовая навигация по кнопкам

--- app/src/main/java/com/example/recepiesapplication/MainActivity.kt
+++ app/src/main/java/com/example/recepiesapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.recepiesapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +22,21 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<CategoriesListFragment>(R.id.mainContainer)
+            }
+        }
+        initUi()
+    }
+
+    fun initUi() {
+        binding.btnCategory.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<CategoriesListFragment>(R.id.mainContainer)
+            }
+        }
+
+        binding.btnFavorites.setOnClickListener {
+            supportFragmentManager.commit {
+                replace<FavoritesFragment>(R.id.mainContainer)
             }
         }
     }
