d58d843e55ef9baee8e4c6cd80996781c71ea3c3
bogdan <tvorbogdan@gmail.com>
2024-02-25 16:23:08 +0200

    RA-05 Исправления

--- app/src/main/java/com/example/recepiesapplication/MainActivity.kt
+++ app/src/main/java/com/example/recepiesapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.recepiesapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -18,24 +17,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         if (savedInstanceState == null) {
-            val bundle = bundleOf("some_int" to 0)
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<CategoriesListFragment>(R.id.mainContainer)
             }
         }
-        initUi()
+        initUI()
     }
 
-    fun initUi() {
+    private fun initUI() {
         binding.btnCategory.setOnClickListener {
             supportFragmentManager.commit {
+                setReorderingAllowed(true)
                 replace<CategoriesListFragment>(R.id.mainContainer)
             }
         }
 
         binding.btnFavorites.setOnClickListener {
             supportFragmentManager.commit {
+                setReorderingAllowed(true)
                 replace<FavoritesFragment>(R.id.mainContainer)
             }
         }
