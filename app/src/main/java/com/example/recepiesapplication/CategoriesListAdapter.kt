25acdca2b44f60c7ebc4312cecac8c70cfa03036
bogdan <tvorbogdan@gmail.com>
2024-03-05 21:11:21 +0200

    RA-08 Categories List Adapter

--- app/src/main/java/com/example/recepiesapplication/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recepiesapplication/CategoriesListAdapter.kt
@@ -5,99 +5,46 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recepiesapplication.CategoriesListAdapter.CategoryViewHolder
 import models.Category
 
-class CategoriesListAdapter :
-    RecyclerView.Adapter<CategoryViewHolder>() {
-
-    private var categories: List<Category> = emptyList()
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): CategoryViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
-        return CategoryViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        holder.bind(categories[position])
-    }
-
-    override fun getItemCount(): Int = categories.size
-
-    fun setCategories(categories: List<Category>) {
-        val result = DiffUtil.calculateDiff(CategoryDiffUtilCallback(this.categories, categories))
-        result.dispatchUpdatesTo(this)
-        this.categories = categories
-    }
-
-    class CategoryViewHolder(
-        view: View
-    ) : RecyclerView.ViewHolder(view) {
-
-        private val imageView: ImageView = itemView.findViewById(R.id.image_category)
-        private val titleTextView: TextView = itemView.findViewById(R.id.tv_title)
-        private val descriptionTextView: TextView = itemView.findViewById(R.id.tv_description)
-        fun bind(item: Category) {
-            imageView.setImageDrawable(null)
-            titleTextView.text = item.title
-            descriptionTextView.text = item.description
-        }
-    }
-
-    private class CategoryDiffUtilCallback(
-        private var oldItems: List<Category>,
-        private var newItems: List<Category>
-    ) : DiffUtil.Callback() {
-        override fun getOldListSize(): Int = oldItems.size
-
-        override fun getNewListSize(): Int = newItems.size
-
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldItems[oldItemPosition].id == newItems[newItemPosition].id
-        }
-
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldItems[oldItemPosition] == newItems[newItemPosition]
-        }
-    }
-}
-
-
-/* class CategoriesListAdapter(private val context: Context, private val dataSet: List<Category>) :
+class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_category, parent, false)
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView,
+        val titleTextView: TextView
+        val descriptionTextView: TextView
+
+        init {
+            imageView = view.findViewById(R.id.image_category)
+            titleTextView = view.findViewById(R.id.tv_title)
+            descriptionTextView = view.findViewById(R.id.tv_description)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_category, viewGroup, false)
+
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val category = dataSet[position]
-        holder.bind(category)
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.titleTextView.text = dataSet[position]
     }
 
+    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView: ImageView = itemView.findViewById(R.id.image_category)
-        val titleTextView: TextView = itemView.findViewById(R.id.tv_title)
-        val descriptionTextView: TextView = itemView.findViewById(R.id.tv_description)
-
-        fun bind(category: Category) {
-            titleTextView.text = category.title
-            descriptionTextView.text = category.description
-            val imageDrawable = AssetUtils.loadImageDrawableFromAssets(context, category.imageUrl)
-            if (imageDrawable != null) {
-                imageView.setImageDrawable(imageDrawable)
-            } else {
-                imageView.setImageDrawable(null)
-            }
-        }
-    }
-} */
+}
